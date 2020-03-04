a739ef4b3539b56a16d2ee190540e9dbeefc06b7
MKDenys <kirichenko.db@gmail.com>
2020-03-04 15:24:51 +0200

    google auth added

--- app/src/main/java/com/example/trackensuretest/MainActivity.java
+++ app/src/main/java/com/example/trackensuretest/MainActivity.java
@@ -1,12 +1,22 @@
 package com.example.trackensuretest;
 
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import com.example.trackensuretest.ui.main.SectionsPagerAdapter;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,7 +30,10 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
+        Button signOutButton = findViewById(R.id.sing_out_button);
+        signOutButton.setOnClickListener(signOutButtonClick);
         registerConnectionReceiver();
+        checkGoogleAccount();
     }
 
     private void registerConnectionReceiver(){
@@ -28,5 +41,34 @@ public class MainActivity extends AppCompatActivity {
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
         registerReceiver(receiver, intentFilter);
+    }
+
+    private void checkGoogleAccount(){
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account == null) {
+            Intent intent = new Intent(this, SignInActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private final View.OnClickListener signOutButtonClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            signOut();
+        }
+    };
+
+    private void signOut(){
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        GoogleSignInClient client = GoogleSignIn.getClient(this, gso);
+        client.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        checkGoogleAccount();
+                    }
+                });
     }
 }
